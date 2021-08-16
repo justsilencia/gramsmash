4ef135fe3b0cd9e9093395f8656de43220271835
justsilencia <silencedogood848@gmail.com>
2021-08-16 15:00:19 -0500

    save changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,39 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
-const IndexPage = () => (
+const IndexPage = () => {
+
+  const handleFile = (e) => {
+    const content = e.target.result;
+    console.log(content);
+  }
+
+  const handleGram = (file) => {
+    let fileData = new FileReader();
+    fileData.onloadend = handleFile;
+    fileData.readAsText(file);
+  }
+
+  return (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Give me gram to smash.</h1>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/smash.jpg"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="A gram smasher..."
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <input type="file" onChange={e => handleGram(e.target.files[0])} />
     </p>
   </Layout>
-)
+  )
+}
 
-export default IndexPage
+export default IndexPage;
